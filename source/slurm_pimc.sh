6d5f0c7e7fd76745e6a68f21ebc4f58f1d2ac322
Brian Chang <34947323+bchangRep@users.noreply.github.com>
2018-12-12 13:11:10 -0500

    Update slurm_pimc.sh

--- source/slurm_pimc.sh
+++ source/slurm_pimc.sh
@@ -11,25 +11,26 @@ echo $SLURM_SUBMIT_DIR
 
 module load mpi/gcc_openmpi
 
-#may need to be edited for proper dir
 cd /scratch
 cp ~/examples/mpi/pi_mc.c ./
 mpicc -o a.out /scratch/pi_mc.c -lm
 
-echo "With 1 processes"
+#Edited to run perfect square of processes
+
+echo "1 processes"
 time mpirun -np 1 --hostfile ./machine_list ./a.out 100000
   
-echo "With 4 processes"
+echo "4 processes"
 time mpirun -np 4 --hostfile ./machine_list ./a.out 100000
   
-echo "With 9 processes"
+echo "9 processes"
 time mpirun -np 9 --hostfile ./machine_list ./a.out 100000
   
-echo "With 16 processes"
+echo "16 processes"
 time mpirun -np 16 --hostfile ./machine_list ./a.out 100000
   
-echo "With 25 processes"
+echo "25 processes"
 time mpirun -np 25 --hostfile ./machine_list ./a.out 100000
   
-echo "With 36 processes"
+echo "36 processes"
 time mpirun -np 36 --hostfile ./machine_list ./a.out 100000
